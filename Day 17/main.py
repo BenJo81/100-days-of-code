42b039ce616bd486fd70cff48e658e5b2ee64db2
Ben Joseph <ben.joseph81@gmail.com>
2024-11-23 14:03:27 -0600

    Day 17

--- Day 17/main.py
+++ Day 17/main.py
@@ -4,6 +4,11 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "Ben")
